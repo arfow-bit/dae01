f5ee45d80e8f153fc6857aa05da4d9204adf8b61
f6u3un-byte <f6u3un@gmail.com>
2025-11-26 15:57:07 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -1,11 +1,26 @@
 "use strict";
 
-const test = (()=>{
-    const init =()=>{
-        console.log("test:init");
+const file = (()=>{
+
+    // aタグを使ったダウンロード
+    const handler_download_json_file = (e)=>{
+
+        const o = { a:100, b:200, };
+
+        let blob = new Blob( [JSON.stringify(o)], { "type": "text/plain" } );
+
+        document.querySelector("#test01").download = "test.json";
+        document.querySelector("#test01").href = window.URL.createObjectURL( blob );
+
+        e.stopPropagation();
     }
+
+    const init =()=>{
+        document.querySelector("#test01").addEventListener("click",handler_download_json_file);
+    }
+
     return {
         init:init,
     }
 })();
-window.addEventListener("load", ()=>{test.init();});
+window.addEventListener("load", ()=>{file.init();});
